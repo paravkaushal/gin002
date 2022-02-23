11ab02dccde4486e6c6e2accedc69df96d17b166
Parav Kaushal <parav.kaushal@velotio.com>
2022-02-23 12:56:40 +0530

    debugging using gin-dump

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -23,7 +24,7 @@ func main() {
 
 	setUpLogOutput()
 	server := gin.New()
-	server.Use(gin.Recovery(), middlewares.Logger())
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, VideoController.FindAll())
