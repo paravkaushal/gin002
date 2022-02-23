5262195eb2286121bc87609905f694f0b1a224f5
Parav Kaushal <parav.kaushal@velotio.com>
2022-02-23 12:36:23 +0530

    save log in a file

--- server.go
+++ server.go
@@ -4,6 +4,8 @@ import (
 	"gin002/controller"
 	"gin002/middlewares"
 	"gin002/service"
+	"io"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,9 +15,14 @@ var (
 	VideoController controller.VideoController = controller.New(VideoService)
 )
 
+func setUpLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
 func main() {
-	server := gin.New()
 
+	setUpLogOutput()
+	server := gin.New()
 	server.Use(gin.Recovery(), middlewares.Logger())
 
 	server.GET("/videos", func(ctx *gin.Context) {
