5ca7b6a5d7d19abd2cd70df6954da97f1f2c2486
Parav Kaushal <parav.kaushal@velotio.com>
2022-02-25 13:42:49 +0530

    html template & multi routing

--- controller/video_controller.go
+++ controller/video_controller.go
@@ -4,6 +4,7 @@ import (
 	"gin002/entity"
 	"gin002/service"
 	"gin002/validators"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
@@ -12,6 +13,7 @@ import (
 type VideoController interface {
 	FindAll() []entity.Video
 	Save(ctx *gin.Context) error
+	ShowAll(ctx *gin.Context)
 }
 type controller struct {
 	service service.VideoService
@@ -42,4 +44,13 @@ func (c *controller) Save(ctx *gin.Context) error {
 	}
 	c.service.Save(video)
 	return nil
+}
+
+func (c *controller) ShowAll(ctx *gin.Context) {
+	videos := c.service.FindAll()
+	data := gin.H{
+		"title":  "Video Page",
+		"videos": videos,
+	}
+	ctx.HTML(http.StatusOK, "index.html", data)
 }
